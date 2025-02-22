9d098550d860c751c1703bb8f96527bdf2b053b4
bacdepzai <ngobac20031016@gmail.com>
2025-02-22 16:35:23 +0700

    update

--- Models/Film/EditFilmViewModel.cs
+++ Models/Film/EditFilmViewModel.cs
@@ -2,8 +2,9 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Netflex.Models.Film;
 
-public class CreateFilmModel
+public class EditFilmViewModel
 {
+    public Guid Id { get; set; }
     [Required, StringLength(100, MinimumLength = 3)]
     public required string Title { get; set; }
     [StringLength(1000)]
@@ -13,10 +14,12 @@ public class CreateFilmModel
     public string? Trailer { get; set; }
     [Required, Range(1, int.MaxValue, ErrorMessage = "Production year must be greater than 0")]
     public int ProductionYear { get; set; }
-    [Required(ErrorMessage = "The Age category field is required.")]
-    public string? AgeCategoryId { get; set; }
+    // [Required(ErrorMessage = "The Age category field is required.")]
+    // public string? AgeCategoryId { get; set; }
     [MaxFileSize(5 * 1024 * 1024), AllowedExtensions([".jpg", ".png", ".jpeg"])]
     public IFormFile? Poster { get; set; }
     [MaxFileSize(5 * 1024 * 1024), AllowedExtensions([".m3u8"])]
     public IFormFile? File { get; set; }
+    public string? PosterUrl { get; set; }
+    public string? FileUrl { get; set; }
 }
