d4655e20be67eaf42ad4d013a3a77ece6a075e0e
thaildhe172591 <121850511+thaildhe172591@users.noreply.github.com>
2025-03-08 07:41:19 +0700

    update

--- Controllers/BlogManagementController.cs
+++ Controllers/BlogManagementController.cs
@@ -1,36 +1,164 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Netflex.Database;
 using Netflex.Models.Blog;
 using System;
 using System.Linq;
+using X.PagedList.Extensions;
+using OfficeOpenXml;
+using System.IO;
 
 namespace Netflex.Controllers
 {
-    public class BlogManagementController : Controller
+    public class BlogManagementController(ApplicationDbContext context,
+    IStorageService storage,
+    IUnitOfWork unitOfWork,
+    ILogger<BlogManagementController> logger)
+    : Controller
     {
-        private readonly ApplicationDbContext _context;
+        private readonly IStorageService _storage = storage;
+        private readonly IUnitOfWork _unitOfWork = unitOfWork;
+        private const int PAGE_SIZE = 5;
+        private readonly ApplicationDbContext _context = context;
 
-        public BlogManagementController(ApplicationDbContext context)
+        private readonly ILogger<BlogManagementController> _logger = logger;
+
+        public IActionResult Index(int? page, string searchTerm, string createrId, DateTime? createdAt, string sortOrder)
         {
-            _context = context;
+            int pageNumber = page ?? 1;
+
+            var blogsQuery = _unitOfWork.Repository<Blog>().Entities.AsQueryable();
+
+            if (!string.IsNullOrEmpty(searchTerm))
+            {
+                blogsQuery = blogsQuery.Where(b => b.Title.Contains(searchTerm));
+            }
+
+            if (!string.IsNullOrEmpty(createrId))
+            {
+                blogsQuery = blogsQuery.Where(b => b.CreaterId == createrId);
+            }
+
+            if (createdAt.HasValue)
+            {
+                blogsQuery = blogsQuery.Where(b => b.CreatedAt.Date == createdAt.Value.Date);
+            }
+
+            switch (sortOrder)
+            {
+                case "title_desc":
+                    blogsQuery = blogsQuery.OrderByDescending(b => b.Title);
+                    break;
+                case "created_at":
+                    blogsQuery = blogsQuery.OrderBy(b => b.CreatedAt);
+                    break;
+                case "created_at_desc":
+                    blogsQuery = blogsQuery.OrderByDescending(b => b.CreatedAt);
+                    break;
+                case "creater_id":
+                    blogsQuery = blogsQuery.OrderBy(b => b.CreaterId);
+                    break;
+                case "creater_id_desc":
+                    blogsQuery = blogsQuery.OrderByDescending(b => b.CreaterId);
+                    break;
+                default:
+                    blogsQuery = blogsQuery.OrderBy(b => b.Title);
+                    break;
+            }
+
+            var models = blogsQuery.Select(b => new BlogViewModel()
+            {
+                Id = b.Id,
+                Title = b.Title,
+                Content = b.Content,
+                Thumbnail = b.Thumbnail,
+                CreatedAt = b.CreatedAt,
+                CreaterId = b.CreaterId,
+            })
+            .ToPagedList(pageNumber, PAGE_SIZE);
+
+            ViewBag.SearchTerm = searchTerm;
+            ViewBag.CreaterId = createrId;
+            ViewBag.CreatedAt = createdAt;
+            ViewBag.SortOrder = sortOrder;
+
+            return View(models);
         }
-
-        public IActionResult Index()
+        public IActionResult ExportToExcel(string searchTerm, string createrId, DateTime? createdAt, string sortOrder)
         {
-            var blogs = _context.Blogs
-                .Select(b => new BlogViewModel
-                {
-                    Id = b.Id,
-                    Title = b.Title,
-                    Content = b.Content,
-                    Thumbnail = b.Thumbnail,
-                    CreatedAt = b.CreatedAt,
-                    CreaterId = b.CreaterId,
-                }).ToList();
+            var blogsQuery = _unitOfWork.Repository<Blog>().Entities.AsQueryable();
 
-            return View(blogs);
+            if (!string.IsNullOrEmpty(searchTerm))
+            {
+                blogsQuery = blogsQuery.Where(b => b.Title.Contains(searchTerm));
+            }
+
+            if (!string.IsNullOrEmpty(createrId))
+            {
+                blogsQuery = blogsQuery.Where(b => b.CreaterId == createrId);
+            }
+
+            if (createdAt.HasValue)
+            {
+                blogsQuery = blogsQuery.Where(b => b.CreatedAt.Date == createdAt.Value.Date);
+            }
+
+            switch (sortOrder)
+            {
+                case "title_desc":
+                    blogsQuery = blogsQuery.OrderByDescending(b => b.Title);
+                    break;
+                case "created_at":
+                    blogsQuery = blogsQuery.OrderBy(b => b.CreatedAt);
+                    break;
+                case "created_at_desc":
+                    blogsQuery = blogsQuery.OrderByDescending(b => b.CreatedAt);
+                    break;
+                case "creater_id":
+                    blogsQuery = blogsQuery.OrderBy(b => b.CreaterId);
+                    break;
+                case "creater_id_desc":
+                    blogsQuery = blogsQuery.OrderByDescending(b => b.CreaterId);
+                    break;
+                default:
+                    blogsQuery = blogsQuery.OrderBy(b => b.Title);
+                    break;
+            }
+
+            var blogsData = blogsQuery.Select(b => new
+            {
+                b.Id,
+                b.Title,
+                b.Content,
+                b.CreaterId,
+                b.CreatedAt
+            }).ToList();
+
+            // Create the Excel file using EPPlus
+            using (var package = new ExcelPackage())
+            {
+                var worksheet = package.Workbook.Worksheets.Add("Blogs");
+
+                worksheet.Cells[1, 1].Value = "ID";
+                worksheet.Cells[1, 2].Value = "Title";
+                worksheet.Cells[1, 3].Value = "Content";
+                worksheet.Cells[1, 4].Value = "Creator ID";
+                worksheet.Cells[1, 5].Value = "Created At";
+
+                for (int i = 0; i < blogsData.Count; i++)
+                {
+                    worksheet.Cells[i + 2, 1].Value = blogsData[i].Id;
+                    worksheet.Cells[i + 2, 2].Value = blogsData[i].Title;
+                    worksheet.Cells[i + 2, 3].Value = blogsData[i].Content;
+                    worksheet.Cells[i + 2, 4].Value = blogsData[i].CreaterId;
+                    worksheet.Cells[i + 2, 5].Value = blogsData[i].CreatedAt;
+                }
+
+                var fileContents = package.GetAsByteArray();
+                return File(fileContents, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "Blogs.xlsx");
+            }
         }
 
         public IActionResult Details(Guid? id)
@@ -38,22 +166,20 @@ namespace Netflex.Controllers
             if (id == null)
                 return NotFound();
 
-            var blog = _context.Blogs
-                .Where(b => b.Id == id)
-                .Select(b => new BlogViewModel
-                {
-                    Id = b.Id,
-                    Title = b.Title,
-                    Content = b.Content,
-                    Thumbnail = b.Thumbnail,
-                    CreatedAt = b.CreatedAt,
-                    CreaterId = b.CreaterId, 
-                }).FirstOrDefault();
-
-            if (blog == null)
+            var b = _unitOfWork.Repository<Blog>().Entities.FirstOrDefault(m => m.Id.Equals(id));
+            if (b == null)
                 return NotFound();
+            var model = new DetailBlogViewModels
+            {
+                Id = b.Id,
+                Title = b.Title,
+                Content = b.Content,
+                Thumbnail = b.Thumbnail,
+                CreatedAt = b.CreatedAt,
+                CreaterId = b.CreaterId,
+            };
 
-            return View(blog);
+            return View(model);
         }
 
         public IActionResult Create()
@@ -64,113 +190,94 @@ namespace Netflex.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Create(BlogViewModel blogViewModel)
+        public async Task<IActionResult> Create(CreateBlogViewModels blogViewModel)
         {
-            if (ModelState.IsValid)
-            {
-                var blog = new Blog
-                {
-                    Id = Guid.NewGuid(),
-                    Title = blogViewModel.Title,
-                    Content = blogViewModel.Content,
-                    Thumbnail = blogViewModel.Thumbnail,
-                    CreatedAt = DateTime.UtcNow,
-                    CreaterId = blogViewModel.CreaterId
-                };
+            if (!ModelState.IsValid)
+                return View(blogViewModel);
 
-                _context.Blogs.Add(blog);
-                _context.SaveChanges();
-                return RedirectToAction(nameof(Index));
+            var thumbnailUri = blogViewModel.Thumbnail != null ? await _storage.UploadFileAsync("thumbnail", blogViewModel.Thumbnail) : null;
+
+            if (blogViewModel.Thumbnail == null)
+            {
+                _logger.LogWarning("No thumbnail uploaded.");
             }
+            else
+            {
+                _logger.LogInformation("Thumbnail uploaded: {FileName}", blogViewModel.Thumbnail.FileName);
+            }
+            var blog = new Blog
+            {
+                Id = Guid.NewGuid(),
+                Title = blogViewModel.Title,
+                Content = blogViewModel.Content,
+                Thumbnail = thumbnailUri?.ToString() ?? string.Empty,
+                CreatedAt = DateTime.UtcNow,
+                CreaterId = blogViewModel.CreaterId
+            };
 
             ViewBag.CreaterId = new SelectList(_context.Users, "Id", "UserName", blogViewModel.CreaterId);
-            return View(blogViewModel);
+            await _unitOfWork.Repository<Blog>().AddAsync(blog);
+            await _unitOfWork.Save(CancellationToken.None);
+
+            return RedirectToAction("index", "blogmanagement");
         }
 
         public IActionResult Edit(Guid? id)
         {
             if (id == null)
                 return NotFound();
-
-            var blog = _context.Blogs.Find(id);
+            var blog = _unitOfWork.Repository<Blog>().Entities.FirstOrDefault(m => m.Id.Equals(id));
             if (blog == null)
                 return NotFound();
 
-            var blogViewModel = new BlogViewModel
+            var blogViewModel = new EditBlogViewModels
             {
                 Id = blog.Id,
                 Title = blog.Title,
                 Content = blog.Content,
-                Thumbnail = blog.Thumbnail,
-                CreatedAt = blog.CreatedAt,
+                ThumbnailUrl = blog.Thumbnail,
+                CreatedAt = DateTime.UtcNow,
                 CreaterId = blog.CreaterId
             };
-
             ViewBag.CreaterId = new SelectList(_context.Users, "Id", "UserName", blog.CreaterId);
             return View(blogViewModel);
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Edit(Guid id, BlogViewModel blogViewModel)
+        public async Task<IActionResult> Edit(EditBlogViewModels blogViewModel)
         {
-            if (id != blogViewModel.Id)
-                return NotFound();
+            if (!ModelState.IsValid)
+                return View(blogViewModel);
 
-            if (ModelState.IsValid)
+            var thumbnailUri = blogViewModel.Thumbnail != null ? await _storage.UploadFileAsync("thumbnail", blogViewModel.Thumbnail) : null;
+            var newBlog = new Blog()
             {
-                var blog = _context.Blogs.Find(id);
-                if (blog == null)
-                    return NotFound();
 
-                blog.Title = blogViewModel.Title;
-                blog.Content = blogViewModel.Content;
-                blog.Thumbnail = blogViewModel.Thumbnail;
-                blog.CreaterId = blogViewModel.CreaterId;
-
-                _context.SaveChanges();
-                return RedirectToAction(nameof(Index));
-            }
-
+                Id = blogViewModel.Id,
+                Title = blogViewModel.Title,
+                Content = blogViewModel.Content,
+                Thumbnail = thumbnailUri?.ToString() ?? blogViewModel.ThumbnailUrl,
+                CreatedAt = DateTime.UtcNow,
+                CreaterId = blogViewModel.CreaterId
+            };
             ViewBag.CreaterId = new SelectList(_context.Users, "Id", "UserName", blogViewModel.CreaterId);
-            return View(blogViewModel);
+            await _unitOfWork.Repository<Blog>().UpdateAsync(newBlog);
+            await _unitOfWork.Save(CancellationToken.None);
+            return RedirectToAction("index", "blogmanagement");
         }
 
-        public IActionResult Delete(Guid? id)
+
+        public async Task<IActionResult> Delete(Guid? id)
         {
             if (id == null)
                 return NotFound();
-
-            var blog = _context.Blogs
-                .Where(b => b.Id == id)
-                .Select(b => new BlogViewModel
-                {
-                    Id = b.Id,
-                    Title = b.Title,
-                    Content = b.Content,
-                    Thumbnail = b.Thumbnail,
-                    CreatedAt = b.CreatedAt,
-                    CreaterId = b.CreaterId,
-                }).FirstOrDefault();
-
-            if (blog == null)
+            var b = _unitOfWork.Repository<Blog>().Entities.FirstOrDefault(m => m.Id.Equals(id));
+            if (b == null)
                 return NotFound();
-
-            return View(blog);
-        }
-
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public IActionResult DeleteConfirmed(Guid id)
-        {
-            var blog = _context.Blogs.Find(id);
-            if (blog != null)
-            {
-                _context.Blogs.Remove(blog);
-                _context.SaveChanges();
-            }
-
-            return RedirectToAction(nameof(Index));
+            await _unitOfWork.Repository<Blog>().DeleteAsync(b);
+            await _unitOfWork.Save(CancellationToken.None);
+            return RedirectToAction("index", "blogmanagement");
         }
     }
 }
