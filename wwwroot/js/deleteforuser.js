cfdec5e6786a182ecedbbf93b716b0c53722b7ea
thaildhe172591 <121850511+thaildhe172591@users.noreply.github.com>
2025-03-25 06:04:22 +0700

    update

---
+++ wwwroot/js/deleteforuser.js
@@ -0,0 +1,64 @@
+$(function () {
+    var url;
+    var redirectUrl;
+    var target;
+
+    $('body').append(`
+        <div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+            <div class="modal-dialog text-white" role="document">
+                <div class="modal-content">
+                    <div class="modal-header pt-2">
+                        <h5 class="modal-title" id="myModalLabel">Warning</h5>
+                        <button class="btn-transparent p-0 fw-bold fs-3 close" type="button" data-bs-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true" class="text-white-50">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body delete-modal-body">  
+                        <!-- Body message here -->
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" id="cancel-delete">Cancel</button>
+                        <button type="button" class="btn btn-danger" id="confirm-delete">Delete</button>
+                    </div>
+                </div>
+            </div>
+        </div>`);
+
+    $(".delete").on('click', (e) => {
+        e.preventDefault();
+
+        target = e.target;
+        var id = $(target).data('id');
+        var controller = $(target).data('controller');
+        var action = $(target).data('action');
+        var bodyMessage = $(target).data('body-message');
+        redirectUrl = $(target).data('redirect-url');
+
+        url = "/" + controller + "/" + action + "/" + id;
+        $(".delete-modal-body").text(bodyMessage);
+        $("#deleteModal").modal('show');
+    });
+
+    $("#confirm-delete").on('click', () => {
+        $.ajax({
+            url: url,
+            type: 'DELETE',
+            success: (result) => {
+                if (!redirectUrl) {
+                    $(target).closest('.home-blog-post').fadeOut("slow");
+                } else {
+                    window.location.href = redirectUrl;
+                }
+            },
+            error: (error) => {
+                console.error("error:", error);
+                if (redirectUrl) {
+                    window.location.href = redirectUrl;
+                }
+            },
+            complete: () => {
+                $("#deleteModal").modal('hide');
+            }
+        });
+    });
+});
