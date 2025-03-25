68a49b7ed521a76820fefa4bbb5de67396c55e3f
bacdepzai <ngobac20031016@gmail.com>
2025-03-25 23:46:33 +0700

    update

--- Database/Configurations/SerieActorConfiguration.cs
+++ Database/Configurations/SerieActorConfiguration.cs
@@ -9,5 +9,9 @@ public class SerieActorConfiguration
     public void Configure(EntityTypeBuilder<SerieActor> builder)
     {
         builder.ToTable("tblSerieActors").HasKey(x => new { x.SerieId, x.ActorId });
+        builder.HasOne<Serie>()
+            .WithMany(s => s.SerieActors)
+            .HasForeignKey(sc => sc.SerieId)
+            .IsRequired();
     }
 }
